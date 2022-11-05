75a1afe36eb0ea43d2c224570afc683fe1f38fde
Satish Patil <satish.patil7017@gmail.com>
2022-11-05 22:50:26 +0530

    Added Create Contact solution

---
+++ AddressBookSystem/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        public void createContact()
+        {
+            Console.WriteLine("1) Enter First Name :");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("2)Enter Last Name");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("3)Enter Address");
+            string Address = Console.ReadLine();
+
+            Console.WriteLine("4)Enter City Name");
+            string City = Console.ReadLine();
+
+            Console.WriteLine("5)Enter State");
+            string State = Console.ReadLine();
+
+            Console.WriteLine("6)Enter Zip Code");
+            int Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("7)Enter Phone Number");
+            long PhoneN0 = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("8)Enter Email-Id");
+            string Email = Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using AddressBookSystem;
+using System;
+
+namespace AddressBooksystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+
+            bool check = true;
+
+            while (check)
+            {
+                Console.WriteLine("Please select the Options :\n" +
+                    "1)Enter Details to add Contact in List\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+
+                        AddressBook addobj = new AddressBook();
+                        addobj.createContact();
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+                check = false;
+                break;
+            }
+        }
+    }
+
+}
